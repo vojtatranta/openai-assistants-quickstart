e0465060b56b42d86c99bbef32b4337bee7e3a6d
Vojtěch Tranta <vojta.tranta@gmail.com>
2024-06-10 16:26:04 +0200

    langtailify

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
   description:
     "A simple app that would should you how to write an AI chat that obtains weather information through Langtail.",
   icons: {
-    icon: "/openai.svg",
+    icon: "https://mintlify.s3-us-west-1.amazonaws.com/langtail-64/_generated/favicon/favicon-32x32.png?v=3",
   },
 };
 
@@ -16,7 +16,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
+        <img
+          className="logo"
+          src="https://mintlify.s3-us-west-1.amazonaws.com/langtail-64/images/logo/light.svg"
+          alt="OpenAI Logo"
+        />
       </body>
     </html>
   );
